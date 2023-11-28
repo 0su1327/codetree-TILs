b62da14439dd06aeb900e8a905b4599c2d026d6b
Youngsu <81498362+0su1327@users.noreply.github.com>
2023-11-28 16:39:30 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231128/합과 차의 나눗셈/divide-of-sum-and-sub.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a,b;
+
+    scanf("%d %d", &a, &b);
+
+    double c = (double)(a + b) / (a - b);
+
+    printf("%.2lf\n", c);
+    return 0;
+}
