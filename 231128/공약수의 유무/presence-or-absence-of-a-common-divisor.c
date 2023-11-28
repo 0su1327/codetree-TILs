bd6d3b9ee146c7b1f6c3c829e979fea1dba66e08
Youngsu <81498362+0su1327@users.noreply.github.com>
2023-11-28 21:49:13 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231128/공약수의 유무/presence-or-absence-of-a-common-divisor.c
+++ 231128/공약수의 유무/presence-or-absence-of-a-common-divisor.c
@@ -8,6 +8,7 @@ int main() {
     scanf("%d %d", &a, &b);
 
     for(int i=a; i<=b; i++){
+        // 1920과 2880의 공약수를 찾기 때문에 두 수에 모두 약분이 되어야한다.
         if(1920%i==0 && 2880%i==0){
             exist = true;
             break;
