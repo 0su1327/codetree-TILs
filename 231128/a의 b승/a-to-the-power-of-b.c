978f29b5bcd020065c93cbfe304b0e94515dd38d
Youngsu <81498362+0su1327@users.noreply.github.com>
2023-11-28 21:12:13 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231128/a의 b승/a-to-the-power-of-b.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%0.lf", pow(a, b));
+
+    
+    return 0;
+}
